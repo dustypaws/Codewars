0d1a5cb616e361a76e87d66a2e7a0be069deda62
dustypaws <sebastian.reichmann@gmx.info>
2019-07-08 15:01:27 +0200

    Done.

--- js/split_strings.js
+++ js/split_strings.js
@@ -1,5 +1,10 @@
 function solution(str) {
-
+    let out = []
+    for (let i = 0; i < str.length; i += 2) {
+        const last = (str[i + 1] === undefined) ? '_' : str[i + 1]
+        out.push(str[i] + last)
+    }
+    return out
 }
-console.log(solution('abc'))
-console.log(solution('abcdef'))
+console.log(solution('abc'), ["ab", "c_"])
+console.log(solution('abcdef'), ["ab", "cd", "ef"])
